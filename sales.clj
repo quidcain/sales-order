e9a114fc9630ce89331ecd921b894e4d33b469ce
quidcain <quidcain@gmail.com>
2018-05-28 00:39:10 +0300

    Add funcs to print customers, products and sales

--- sales.clj
+++ sales.clj
@@ -32,3 +32,29 @@
 (def customers (sort-by :custID (map parse-cust (slurp-and-parse "cust.txt"))))
 (def products (sort-by :prodID (map parse-prod (slurp-and-parse "prod.txt"))))
 (def sales (sort-by :salesID (map parse-sales (slurp-and-parse "sales.txt"))))
+
+(defn display-cust-table
+  [customers]
+  (for [customer customers]
+    (println (format "%d: [\"%s\" \"%s\" \"%s\"]"
+                 (:custID customer)
+                 (:name customer)
+                 (:address customer)
+                 (:phoneNumber customer)))))
+
+(defn display-prod-table
+  [products]
+  (for [product products]
+    (println (format "%d: [\"%s\" \"%s\"]"
+                 (:prodID product)
+                 (:itemDescription product)
+                 (:unitCost product)))))
+
+(defn display-sales-table
+  [sales customers products]
+  (for [sale sales]
+    (println (format "%d: [\"%s\" \"%s\" \"%d\"]"
+                 (:salesID sale) 
+                 (:name (first (filter #(= (:custID %)(:custID sale)) customers)))
+                 (:itemDescription (first (filter #(= (:prodID %)(:prodID sale)) products)))
+                 (:itemCount sale)))))
